ae94cc4b0e0ce008d0728335f1991ab303324af5
Arbaz Khan <arbazkha78az@gmail.com>
2025-12-31 17:22:56 +0530

    setting up login and signup functionality and set token in localStorage

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,15 +1,21 @@
-import React, { useEffect, useState } from 'react'
-import "./LoginPopup.css"
+import React, { useContext, useState } from 'react';
+import axios from 'axios';
+import "./LoginPopup.css";
+import { toast } from 'react-toastify';
 import { assets } from '../../assets/assets';
+import { StoreContext } from '../../context/StoreContext';
 
 const LoginPopup = ({setShowLogin}) => {
+  const { url, token, setToken } = useContext(StoreContext);
   const [currentState, setCurrentState] = useState("Login");
+
   const [data, setData] = useState({
     name: "",
     email: "",
     password: ""
   });
 
+  //Input change handler
   const onChangeHandler = (e) => {
     const name = e.target.name;
     const value = e.target.value;
@@ -17,13 +23,38 @@ const LoginPopup = ({setShowLogin}) => {
     setData(data => ({...data, [name]: value}));
   }
 
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
+  // handle login and Signup
+  const onLogin = async (e) => {
+    e.preventDefault();
+
+    let newURL = url;
+    if(currentState === "Login") {
+      newURL += "/api/user/login"
+    }
+    else if(currentState === "Sign up") {
+      newURL += "/api/user/register"
+    }
+    else {
+      console.log("Error while login");
+    }
+
+    
+    const response = await axios.post(newURL, data);
+    
+    if(response.data.success) {
+      setToken(response.data.token);
+      localStorage.setItem("token", response.data.token);
+      setShowLogin(false);
+      toast.success("Logout success");
+    }
+    else {
+      toast.error(response.data.message);
+    }
+  }
 
   return (
     <div className='login-popup'>
-      <form action="" className="login-popup-container">
+      <form onSubmit={onLogin} className="login-popup-container">
         <div className='login-popup-title'>
           <h2>{currentState}</h2>
           <img name="image" onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
@@ -33,10 +64,10 @@ const LoginPopup = ({setShowLogin}) => {
           <input name="email" value={data.email} onChange={onChangeHandler} type="email" placeholder='Your email' required/>
           <input name="password" value={data.password} onChange={onChangeHandler} type="password" placeholder='Your password' required/>
         </div>
-        <button>{currentState === "Sign up" ? "Create account" : "Login"}</button>
+        <button type='submit'>{currentState === "Sign up" ? "Create account" : "Login"}</button>
         <div className='login-popup-condition'>
-          <input type="checkbox" required/>
-          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+          <input id="checkbox" type="checkbox" required/>
+          <label for="checkbox">By continuing, i agree to the terms of use & privacy policy.</label>
         </div>
         {currentState === "Login"
           ? <p>Create a new account? <span onClick={() => setCurrentState("Sign up")}>Click here</span></p>
