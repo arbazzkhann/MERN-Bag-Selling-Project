5e8387d92c8848c040d8f56e73658a20d551cc71
Arbaz Khan <arbazkha78az@gmail.com>
2025-12-31 14:08:36 +0530

    cotrolling form and fixing signin and signup issue

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,21 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import "./LoginPopup.css"
 import { assets } from '../../assets/assets';
 
 const LoginPopup = ({setShowLogin}) => {
-  const [currentState, setCurrentState] = useState("Sign up");
+  const [currentState, setCurrentState] = useState("Login");
+  const [data, setData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  const onChangeHandler = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setData(data => ({...data, [name]: value}));
+  }
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   return (
     <div className='login-popup'>
       <form action="" className="login-popup-container">
         <div className='login-popup-title'>
           <h2>{currentState}</h2>
-          <img onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
+          <img name="image" onClick={() => setShowLogin(false)} src={assets.cross_icon} alt="" />
         </div>
         <div className='login-popup-input'>
-          {currentState === "Sign up" ? <></> : <input type="text" placeholder='Your name' required/>}
-          <input type="email" placeholder='Your email' required/>
-          <input type="password" placeholder='Your password' required/>
+          {currentState === "Sign up" ? <input name="name" value={data.name} onChange={onChangeHandler} type="text" placeholder='Your name' required/> : <></>}
+          <input name="email" value={data.email} onChange={onChangeHandler} type="email" placeholder='Your email' required/>
+          <input name="password" value={data.password} onChange={onChangeHandler} type="password" placeholder='Your password' required/>
         </div>
         <button>{currentState === "Sign up" ? "Create account" : "Login"}</button>
         <div className='login-popup-condition'>
