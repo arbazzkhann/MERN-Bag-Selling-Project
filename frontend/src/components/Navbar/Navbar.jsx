dbb94173958805991c567b54ed607b4ffb601a62
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-04 12:26:48 +0530

    adding my-orders in navbar

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -39,7 +39,7 @@ const Navbar = ({setShowLogin}) => {
         : <div className="navbar-profile">
             <img src={assets.profile_icon} alt="" />
             <ul className="navbar-profile-dropdown">
-              <li><img src={assets.bag_icon} alt="" />Orders</li>
+              <Link to='/my-orders'><li><img src={assets.bag_icon} alt="" />Orders</li></Link>
               <hr />
               <li onClick={logout}><img src={assets.logout_icon} alt="" />Logout</li>
             </ul>
