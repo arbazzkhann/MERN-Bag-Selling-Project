7a5f34c5f45c9e361c000c437d85824f9500d2d2
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-04 13:09:33 +0530

    Making order page for admin

--- admin/src/pages/Orders/Orders.jsx
+++ admin/src/pages/Orders/Orders.jsx
@@ -1,9 +1,66 @@
 import React from 'react';
 import './Orders.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+import { assets } from '../../assets/assets.js';
 
 const Orders = ({url}) => {
+    const [orders, setOrders] = useState([]);
+
+    const fetchAllOrders = async () => {
+        const response = await axios.post(`${url}/api/order/list`);
+        if(response.data.success) {
+            setOrders(response.data.data);
+            console.log(response.data.data);
+        }
+        else {
+            toast.error("Error");
+        }
+    }
+
+    useEffect(() => {
+        fetchAllOrders();
+    }, []);
+
   return (
-    <div>Orders</div>
+    <div className="order add">
+        <h3>Order Page</h3>
+        <div className="order-list">
+            {orders.map((order, index) => {
+                return (
+                    <div key={index} className='order-item'>
+                        <img src={assets.parcel_icon} alt="" />
+                        <div>
+                            <p className="order-item-bag">
+                                {order.items.map((item, index) => {
+                                    if(index === order.items.length - 1) {
+                                        return `${item.name} x ${item.quantity}`; // for last item "without last comma"
+                                    }
+                                    else {
+                                        return `${item.name} x ${item.quantity}, `; // for all items except last item
+                                    }
+                                })}
+                            </p>
+                            <p className='order-item-name'>{`${order.address.firstName} ${order.address.lastName}`}</p>
+                            <p className="order-item-address">
+                                <p>{`${order.address.street}, `}</p>
+                                <p>{`${order.address.city}, ${order.address.state}, ${order.address.country}, ${order.address.pincode}`}</p>
+                            </p>
+                        </div>
+                        <p className='order-item-phone'>{order.address.phone}</p>
+                        <p>Items: {order.items.length}</p>
+                        <p>₹{order.amount}</p>
+                        <select name="status">
+                            <option value="Processing">Processing</option>
+                            <option value="Out for delivery">Out for delivery</option>
+                            <option value="Delivered">Delivered</option>
+                        </select>
+                    </div>
+                )
+            })}
+        </div>
+    </div>
   )
 }
 
