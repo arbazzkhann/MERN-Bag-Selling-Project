3d4f01645b93bdc5b55e4dd832729860e1dc6b77
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-05 20:18:51 +0530

    adding update product functionality

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,68 +1,173 @@
-import React from 'react'
-import './List.css'
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-import { toast } from 'react-toastify'
+import React from "react";
+import "./List.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { FaEdit } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
-const List = ({url}) => {
-    const [ list, setList ] = useState([]);
+const List = ({ url }) => {
+    const [list, setList] = useState([]);
+    const [isEdit, setIsEdit] = useState(false);
+    const [editId, setEditId] = useState(null);
+
+    const [data, setData] = useState({
+        name: "",
+        category: "",
+        mrp: "",
+        price: "",
+    });
 
     const fetchList = async () => {
         const response = await axios.get(`${url}/api/bag/list`);
 
-        if(response.data.success === true) {
+        if (response.data.success === true) {
             setList(response.data.data);
-        }
-        else {
+        } else {
             toast.error("Error while fetching :(");
         }
-
-        console.log(response.data.data)
-    }
+    };
 
     const removeBag = async (id) => {
-        const response = await axios.delete(`${url}/api/bag/remove`, {data: {id}});
+        const response = await axios.delete(`${url}/api/bag/remove`, {
+            data: { id },
+        });
 
         await fetchList();
-        
-        if(!response.data.success) {
-            return toast.error(response.data.message)
+
+        if (!response.data.success) {
+            return toast.error(response.data.message);
         }
 
-        return toast.success("Error")
-    }
+        return toast.success("Product removed");
+    };
 
     useEffect(() => {
         fetchList();
-    }, [])
+    }, [data]);
+
+    const onEditHandler = async (id) => {
+        const response = await axios.post(`${url}/api/bag/get-by-id`, { id });
+
+        setData({
+            name: response.data.data.name,
+            category: response.data.data.category,
+            mrp: response.data.data.mrp,
+            price: response.data.data.price,
+        });
+
+        setEditId(id);
+        setIsEdit(true);
+    };
+
+    const onChangeHandler = (e) => {
+        const { name, value } = e.target;
+
+        setData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    const updateBag = async (id) => {
+        const response = await axios.put(`${url}/api/bag/update`, {id, data});
+
+        if(response.data.success) {
+            toast.success("Product successfully updated.");
+        }
+        else {
+            toast.error("Error while update.");
+        }
+
+        setIsEdit(false);
+        fetchList() //fetching all list
+    }
 
     return (
-        <div className='list add flex-col'>
+        <div className="list add flex-col">
             <p>All Bags List</p>
             <div className="list-table">
-                 <div className="list-table-format title">
+                <div className="list-table-format title">
                     <b>Image</b>
                     <b>Name</b>
                     <b>Category</b>
                     <b>MRP</b>
                     <b>Price</b>
                     <b>Action</b>
-                 </div>
-                 {list.map((item, index) => {
-                    return (
-                        <div key={index} className='list-table-format'>
-                            <img src={`${url}/images/` + item.image} alt="" />
-                            <p>{item.name}</p>
-                            <p>{item.category}</p>
-                            <p>{item.mrp}</p>
-                            <p>{item.price}</p>
-                            <p onClick={() => removeBag(item._id)} className='cursor'>X</p>
-                        </div>
-                    )
-                 })}
-            </div>            
-        </div>
-    )
-}
+                </div>
+                {list.map((item) => (
+                    <div key={item._id} className="list-table-format">
+                        <img src={`${url}/images/${item.image}`} alt="" />
 
-export default List
+                        {isEdit && editId === item._id ? (
+                            <input name="name" value={data.name} onChange={onChangeHandler} />
+                        ) : (
+                            <p>{item.name}</p>
+                        )}
+
+                        {isEdit && editId === item._id ? (
+                            <select onChange={onChangeHandler} value={data.category} name="category" required>
+                                <option value="" disabled selected>Select</option>
+                                <option value="Backpacks">Backpacks</option>
+                                <option value="Handbags">Handbags</option>
+                                <option value="Laptop Bags">Laptop Bags</option>
+                                <option value="Travel Bags">Travel Bags</option>
+                                <option value="GYM Bags">GYM Bags</option>
+                                <option value="School Bags">School Bags</option>
+                                <option value="Camera Bags">Camera Bags</option>
+                            </select>
+                        ) : (
+                            <p>{item.category}</p>
+                        )}
+
+                        {isEdit && editId === item._id ? (
+                            <input 
+                                type="number" 
+                                name="mrp" 
+                                value={data.mrp} 
+                                onChange={onChangeHandler} 
+                            />
+                        ) : (
+                            <p>{item.mrp}</p>
+                        )}
+
+                        {isEdit && editId === item._id ? (
+                            <input
+                                 type="number"
+                                name="price"
+                                value={data.price}
+                                onChange={onChangeHandler}
+                            />
+                        ) : (
+                            <p>{item.price}</p>
+                        )}
+
+                        <div>
+                            {isEdit && editId === item._id ? (
+                                <>
+                                    <button onClick={() => setIsEdit(false)} className="button">
+                                        Cancel
+                                    </button>
+                                    <button onClick={() => updateBag(item._id)} className="button">Update</button>
+                                </>
+                            ) : (
+                                <button
+                                    onClick={() => onEditHandler(item._id)}
+                                    className="button"
+                                >
+                                    <FaEdit />
+                                </button>
+                            )}
+
+                            <button onClick={() => removeBag(item._id)} className="button">
+                                <MdDelete />
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default List;
