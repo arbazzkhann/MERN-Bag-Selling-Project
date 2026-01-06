a9c758cdb97c8c20b054f4808ebf98594ec2ac65
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-06 20:41:36 +0530

    commenting env

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import "dotenv/config.js";
+// import "dotenv/config.js";
 
 import { connectDB } from "./config/db.js";
 import bagRouter from "./routes/bag.route.js";
