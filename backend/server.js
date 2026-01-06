cae4c27036522c834f3caf15540b077dd3889681
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-06 18:12:02 +0530

    cors

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 
 //middleware
 app.use(express.json());
-app.use(cors("http://localhost:5173"));
+app.use(cors("https://mern-bag-selling-project.onrender.com"));
 
 //db connection
 connectDB();
