9f5c1b3212fd527de6d101ac3fa0ad63b46dcd53
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-08 22:43:03 +0530

    Updating ExploreBags UI with CSS and wrapping with div

--- frontend/src/components/ExploreBags/ExploreBags.jsx
+++ frontend/src/components/ExploreBags/ExploreBags.jsx
@@ -4,20 +4,19 @@ import { bag_type_list } from '../../assets/assets'
 
 const ExploreBags = ({category, setCategory, heading}) => {
   return (
-    <div className='explore-products' id='explore-products'>
-        <h1>{heading}</h1>
-        {/* <p className='explore-products-text'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iure, beatae similique atque debitis quo autem reiciendis amet repudiandae labore? Non.</p> */}
+    <div className="explore-products-list-wrapper">
         <div className="explore-products-list">
-            {bag_type_list.map((item, index) => {
-                return (
-                    <div onClick={() => setCategory(prev => prev === item.type ? "All" : item.type)} className='explore-products-list-item' key={index}>
-                        <img className={category === item.type ? "active" : ""} src={item.type_image} alt="" />
-                        <p>{item.type}</p>
-                    </div>
-                );
-            })}
+            {bag_type_list.map((item, index) => (
+                <div 
+                    onClick={() => setCategory(prev => prev === item.type ? "All" : item.type)} 
+                    className='explore-products-list-item' 
+                    key={index}
+                >
+                    <img className={category === item.type ? "active" : ""} src={item.type_image} alt="" />
+                    <p>{item.type}</p>
+                </div>
+            ))}
         </div>
-        <hr />
     </div>
   )
 }
