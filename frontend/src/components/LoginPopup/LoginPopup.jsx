29d76097851ea440494410f2dcfd5eb9cbb43aca
Arbaz Khan <arbazkha78az@gmail.com>
2026-01-09 16:26:30 +0530

    Changing login success message

--- frontend/src/components/LoginPopup/LoginPopup.jsx
+++ frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -45,7 +45,7 @@ const LoginPopup = ({setShowLogin}) => {
       setToken(response.data.token);
       localStorage.setItem("token", response.data.token);
       setShowLogin(false);
-      toast.success("Logout success");
+      toast.success("Login success");
     }
     else {
       toast.error(response.data.message);
